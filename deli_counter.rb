e4a14dd8f9278edac77b0d70b96e327f20a785b8
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-16 01:55:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+  else 
+    line_status = "The line is currently:"
+    katz_deli.each.with_index(1) {|name, index| line_status << " #{index}. #{name}"}
+      puts line_status
+  end 
+end
+
+
+
+
