1961b996b1feec9837256e215743df3177b2f662
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-16 02:03:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ end
 
 def take_a_number(line, name)
  katz_deli.push(name)
- puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+ puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 
