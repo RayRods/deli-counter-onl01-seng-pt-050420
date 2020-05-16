187f36215432b49a82b45bc54606615906068d9a
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-16 02:20:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,12 +9,10 @@ def line(katz_deli)
   end 
 end
 
-
 def take_a_number(line, name)
  line.push(name)
  puts "Welcome, #{name}. You are number #{line.length} in line."
 end
-
 
 
 def now_serving(katz_deli)
